5c53c18a04b1c3cd695bf76e50f2d687991588c6
Caleb Phillips <csphillips5@gmail.com>
2021-09-30 12:03:42 -0400

    7.8 implement custom hook 'useResource' to handle fetching all/creating resources

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-  
-import React, { useState, useEffect } from 'react'
+
+import React, { useState, useEffect, useCallback } from 'react'
 import axios from 'axios'
 
 
@@ -20,14 +20,23 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  const getAll = useCallback(async () => {
+    const response = await axios.get(baseUrl)
+    setResources(response.data)
+  }, [baseUrl])
 
-  const create = (resource) => {
-    // ...
+  useEffect(() => {
+    getAll()
+  }, [getAll])
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources([...resources, response.data])
   }
 
   const service = {
-    create
+    create,
+    getAll
   }
 
   return [
@@ -47,7 +56,7 @@ const App = () => {
     event.preventDefault()
     noteService.create({ content: content.value })
   }
- 
+
   const handlePersonSubmit = (event) => {
     event.preventDefault()
     personService.create({ name: name.value, number: number.value})
